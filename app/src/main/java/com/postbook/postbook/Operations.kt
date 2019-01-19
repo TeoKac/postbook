eb867ec972fc4af4f3e30aa56491b7a7be25684c
Teo <kac.teo@gmail.com>
2019-01-19 12:56:48 +0100

    Celoten projekt za RVIR

---
+++ app/src/main/java/com/postbook/postbook/Operations.kt
@@ -0,0 +1,30 @@
+package com.postbook.postbook
+
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+
+class Operations{
+
+    fun ConvertStreamToString(inputStream: InputStream):String{
+
+        val bufferReader= BufferedReader(InputStreamReader(inputStream))
+        var line:String
+        var AllString:String=""
+
+        try {
+            do{
+                line=bufferReader.readLine()
+                if(line!=null){
+                    AllString+=line
+                }
+            }while (line!=null)
+            inputStream.close()
+        }catch (ex:Exception){}
+
+
+
+        return AllString
+    }
+
+}
